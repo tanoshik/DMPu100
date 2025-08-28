1b86199fa5871e9c0e9514ae3aee8f305be83e17
tanoshik <tanosso@gmail.com>
2025-08-28 23:35:41 +0900

    gitignore update

--- src/.rcpp_cache/sourceCpp-x86_64-w64-mingw32-1.1.0/sourcecpp_2bd45e70610e/matcher_fast.cpp.R
+++
@@ -1,5 +0,0 @@
-`.sourceCpp_1_DLLInfo` <- dyn.load('C:/Users/tanos/projects/DMPu100/src/.rcpp_cache/sourceCpp-x86_64-w64-mingw32-1.1.0/sourcecpp_2bd45e70610e/sourceCpp_13.dll')
-
-compute_scores_uint16 <- Rcpp:::sourceCppFunction(function(q1, q2, r1, r2, any_code = 9999L) {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_compute_scores_uint16')
-
-rm(`.sourceCpp_1_DLLInfo`)
